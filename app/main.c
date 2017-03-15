45d636ff7fe52167811ff79ca006e6392181f793
Sean Nyekjaer <sean.nyekjaer@prevas.dk>
2017-03-15 21:25:47 +0100

    initial commit

---
+++ app/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello World!");
+
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+    return 0;
+}
